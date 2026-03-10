fdcc3126c4e0462ae78f16278cb97be7e0a3bad5
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-10 13:17:01 +0900

    [fix]リポジトリ名に合わせてURLを修正

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://hibiki5201.github.io',
-  base: '/', 
+  base: '/HIBIKI5201', // リポジトリ名に合わせて修正
   trailingSlash: 'always',
   build: {
     format: 'directory',
